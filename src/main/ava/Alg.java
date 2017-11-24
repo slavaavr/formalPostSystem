231f8bc08b7d265b45fff428da59acade31b3256
AVA <_AVA@outlook.com>
2017-11-24 11:29:07 +0300

    v 2

--- src/main/ava/Alg.java
+++ src/main/ava/Alg.java
@@ -30,76 +30,74 @@ public class Alg {
 
     public void run() throws IOException {
         Map<String, Set<String>> map_var_val = new HashMap<>();
-        boolean thereAreValidRules;
         Character c_line;
         Character c_rule;
         StringBuilder valueOfVariable;
         int i, j, offset;
-        do {
-            thereAreValidRules = false;
-            for (Pair<String, String> rule : rules) {
-                j = 0;
-                offset = 0;
-                newRule:
-                for (i = 0; i < rule.getKey().length(); i++) {
-                    c_rule = rule.getKey    ().charAt(i);
-                    if (isItVariable(c_rule)) {
-                        if (!map_var_val.containsKey(Character.toString(c_rule)))
-                            map_var_val.put(Character.toString(c_rule), new HashSet<>());
-                        valueOfVariable = new StringBuilder();
-                        for (; j < this.line.length(); j++) {
-                            c_line = this.line.charAt(j);
-                            if (isItAxiom(c_line)) {
-                                valueOfVariable.append(c_line);
-                                if (j == this.line.length() - 1) {
-                                    map_var_val.get(Character.toString(c_rule)).add(valueOfVariable.toString());
-                                }
-                            } else if (j == offset) {
-                                map_var_val.clear();
-                                break newRule;
-                            } else {
-                                if (valueOfVariable.length() != 0)
-                                    map_var_val.get(Character.toString(c_rule)).add(valueOfVariable.toString());
-                                break;
+
+        for (int k = 0; k < rules.size(); k++) {
+            Pair<String, String> rule = rules.get(k);
+            j = 0;
+            offset = 0;
+            newRule:
+            for (i = 0; i < rule.getKey().length(); i++) {
+                c_rule = rule.getKey().charAt(i);
+                if (isItVariable(c_rule)) {
+                    if (!map_var_val.containsKey(Character.toString(c_rule)))
+                        map_var_val.put(Character.toString(c_rule), new HashSet<>());
+                    valueOfVariable = new StringBuilder();
+                    for (; j < this.line.length(); j++) {
+                        c_line = this.line.charAt(j);
+                        if (isItAxiom(c_line)) {
+                            valueOfVariable.append(c_line);
+                            if (j == this.line.length() - 1) {
+                                map_var_val.get(Character.toString(c_rule)).add(valueOfVariable.toString());
                             }
-                        }
-                    } else {
-                        if (c_rule != this.line.charAt(j)) {
+                        } else if (j == offset) {
                             map_var_val.clear();
-                            break;
+                            break newRule;
                         } else {
-                            j++;
+                            if (valueOfVariable.length() != 0)
+                                map_var_val.get(Character.toString(c_rule)).add(valueOfVariable.toString());
+                            break;
                         }
                     }
-                    if (i == rule.getKey().length() - 1) {
-                        boolean flag = false;
-                        for (Set<String> set : map_var_val.values()) {
-                            if (set.size() != 1) {
-                                map_var_val.clear();
-                                i = -1;
-                                offset++;
-                                j = offset;
-                                flag = true;
-                                break;
-                            }
-                        }
-                        if (!flag) {
-                            String key = rule.getKey();
-                            String value = rule.getValue();
-                            for (Map.Entry<String, Set<String>> setEntry : map_var_val.entrySet()) {
-                                key = key.replaceAll(setEntry.getKey(), setEntry.getValue().toArray()[0].toString());
-                                value = value.replaceAll(setEntry.getKey(), setEntry.getValue().toArray()[0].toString());
-                            }
-                            this.line = this.line.replace(key, value);
-                            fw.write(this.line + "\n");
-                            fw.flush();
+                } else {
+                    if (c_rule != this.line.charAt(j)) {
+                        map_var_val.clear();
+                        break;
+                    } else {
+                        j++;
+                    }
+                }
+                if (i == rule.getKey().length() - 1) {
+                    boolean flag = false;
+                    for (Set<String> set : map_var_val.values()) {
+                        if (set.size() != 1) {
                             map_var_val.clear();
-                            thereAreValidRules = true;
+                            i = -1;
+                            offset++;
+                            j = offset;
+                            flag = true;
+                            break;
                         }
+                    }
+                    if (!flag) {
+                        String key = rule.getKey();
+                        String value = rule.getValue();
+                        for (Map.Entry<String, Set<String>> setEntry : map_var_val.entrySet()) {
+                            key = key.replaceAll(setEntry.getKey(), setEntry.getValue().toArray()[0].toString());
+                            value = value.replaceAll(setEntry.getKey(), setEntry.getValue().toArray()[0].toString());
+                        }
+                        this.line = this.line.replace(key, value);
+                        fw.write(this.line + "\n");
+                        fw.flush();
+                        map_var_val.clear();
+                        k--;
                     }
                 }
             }
-        } while (thereAreValidRules);
+        }
     }
 
     private boolean isItAxiom(Character c) {
